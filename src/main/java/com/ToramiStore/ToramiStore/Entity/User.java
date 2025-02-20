650671d391fc71c9c594ab2a776f667548d45016
Jeazoal <alonsomarzano@gmail.com>
2025-02-19 21:09:24 -0500

    Se modifico la entidad user para poder manejar tokens y asi la verificacion de gmail

--- src/main/java/com/ToramiStore/ToramiStore/Entity/User.java
+++ src/main/java/com/ToramiStore/ToramiStore/Entity/User.java
@@ -1,40 +1,51 @@
 package com.ToramiStore.ToramiStore.Entity;
 
 import jakarta.persistence.*;
-import lombok.*;
-import org.springframework.transaction.annotation.Transactional;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
+import java.util.UUID;
 
-//uwu
-@Data
-@AllArgsConstructor
-@NoArgsConstructor
 @Entity
-@Transactional
-@ToString
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 @Table(name = "users")
 public class User implements Serializable {
+
     @Id
-    @Column(name="id_user")
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer iduser;
-    @Column(name="nombre")
+    @Column(name = "id_user")
+    private Integer idUser;
+
+    @Column(name = "nombre", nullable = false)
     private String nombre;
-    @Column(name="apellido")
-    private String apellido;
-    @Column(name="correo")
+
+    @Column(name = "apellidos", nullable = false)
+    private String apellidos;
+
+    @Column(name = "correo", unique = true, nullable = false)
     private String correo;
-    @Column(name="password")
+
+    @Column(name = "password", nullable = false)
     private String password;
-    @Column(name="dni")
-    private int dni;
-    @Column(name="departamento")
-    private String departamento;
-    @Column(name="distrito")
-    private String distrito;
-    @Column(name="domicilio")
-    private String domicilio;
-    @Column(name="numero")
-    private int numero;
+
+    @Column(name = "dni", unique = true)
+    private String dni;
+
+    @Column(name = "numero", unique = true)
+    private String numero;
+
+    @Column(name = "activo")
+    private Boolean activo = false;
+
+    @Column(name = "token")
+    private String verificationToken;
+
+    public void generateVerificationToken() {
+        this.verificationToken = UUID.randomUUID().toString();
+    }
 }
+
